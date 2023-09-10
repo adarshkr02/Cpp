4ad43e19261abf3a93db6625a50a3c2cb5811822
ayan-0203 <adarsh02kr@gmail.com>
2023-09-10 12:32:55 +0530

    AbstractClass

---
+++ Abstractclass1.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream>
+
+class Person {
+public:
+  virtual void fun() = 0; // Pure Virtual Function --> (do Nothing function)
+  void f1() {}
+};
+
+class Student : public Person {
+public:
+  void fun() {}
+};
